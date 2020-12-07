3ef62c1e748d830d7200b7ad36e5a4c448c9cfa8
yoshimuratakuma <takuma.yoshimura@revcomm.co.jp>
2020-12-07 20:14:20 +0900

    add test with stub

--- app/src/androidTest/java/com/example/uitest_espresso/utils/OperatorTest.kt
+++ app/src/androidTest/java/com/example/uitest_espresso/utils/OperatorTest.kt
@@ -1,5 +1,6 @@
 package com.example.uitest_espresso.utils
 
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.After
 import org.junit.Before
 
@@ -10,13 +11,24 @@ import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class OperatorTest {
+    lateinit var target: Operator
 
     @Before
     fun setUp() {
+        target = Operator()
     }
 
     @After
     fun tearDown() {
     }
 
+    @Test
+    fun calcTextNumber_given_validValues_returnsSuccess() {
+        val stubValidator = StubValidator(isNumber = true, isOperator = true)
+        val input = Operator.InputNumbers("11", "100", "+")
+
+        val actual = Operator().calcTextNumber(input, stubValidator)
+        assertThat(actual.first == Operator.CalcResult.SUCCESS, `is`(true))
+        assertThat(actual.second == "111", `is`(true))
+    }
 }
